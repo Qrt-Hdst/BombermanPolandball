bc7a29f923d30e1ab988c6c0dcb5e9a757c929db
Damian <DamianOrzel19@gmail.com>
2019-02-16 15:03:07 +0100

    audio

---
+++ main.js
@@ -0,0 +1,17 @@
+const cvs = document.getElementById('snake');
+const ctx = cvs.getContext("2d");
+
+const box = 32;
+
+const ground = new Image();
+
+
+
+let dead = new Audio();
+let eat = new Audio();
+let up = new Audio();
+let right = new Audio();
+let left = new Audio();
+let down = new Audio();
+
+
